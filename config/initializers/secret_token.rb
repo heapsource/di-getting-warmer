3b1e3a4107bc4a066c49f27dc55d3ad2460ca134
Johan Hernández <johan@firebase.co>
2013-06-22 10:08:56 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GettingWarmer::Application.config.secret_token = '2ce10ce87256acf918243e0ce4fdc2083858dda5d333dc1390e86745ab6f3729fc150a9241f858db2c7963e0b885ef46700f369a3ef5f559519e4d0da1c0aa83'
